cff558f7fd4326b755e12f80557b250e46df9614
CODExR <106008744+rahul-p-007@users.noreply.github.com>
2025-04-13 17:09:37 +0530

    Add files via upload

---
+++ DSA-Questions/String/RemoveDuplicate.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+string RemoveDuplicate(string s) {
+    string ans = "";
+    int i = 0;
+    while(i< s.length()){
+        if(ans.length() > 0){
+            if(ans[ans.length()-1] == s[i]){
+                ans.pop_back();
+            }else{
+                ans.push_back(s[i]);
+            }
+        }else{
+          ans.push_back(s[i]);
+        }
+        i++;
+    }
+    return ans;
+}
+int main() {
+    vector<string> testCases = {
+        "abbaca",     // Expected: "ca"
+        "azxxzy",     // Expected: "ay"
+        "aabccb",     // Expected: "b"
+        "aaaa",       // Expected: ""
+        "abcddcba",   // Expected: ""
+        "",           // Expected: ""
+        "abccba"      // Expected: ""
+    };
+
+    for (string test : testCases) {
+        cout << "Input: \"" << test << "\" => Output: \"" << RemoveDuplicate(test) << "\"" << endl;
+    }
+
+    return 0;
+}
